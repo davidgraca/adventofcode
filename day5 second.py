85c5f177f5a08d1fefe5490a59fe59dd85d1bb4c
David Graça <davidgraca@gmail.com>
2020-12-09 20:56:10 +0000

    ouf

---
+++ day5 second.py
@@ -0,0 +1,34 @@
+
+def seatId(data):
+    up = 0
+    low = 128
+
+    for c in data[:len(data)-3]:
+        if(c=='F'):low= low - (low-up)/2
+        if(c=='B'):up= up +(low - up)/2
+        if(c=='L'):up= (up - low)/2
+        if(c=='R'):up= (up - low)/2
+
+    #print(up, low-1)
+
+    lseat=0
+    hseat=8
+
+    for c in data[len(data)-3:]:
+        if(c=='L'):hseat= hseat - (hseat-lseat)/2
+        if(c=='R'):lseat= lseat +(hseat - lseat)/2
+    #print(up, hseat-1)
+    return (up*8)+hseat-1
+
+
+mylines = []                             # Declare an empty list named mylines.
+with open ('datad5.txt', 'rt') as myfile:
+    lines= list(myfile)
+    mylines= map(lambda x: seatId(x.replace("\n","")),lines)
+
+values= list(mylines)
+dict = {}
+
+missing_seat = (max(values) * (max(values) + 1) / 2) - ((min(values) - 1) * min(values) / 2) - sum(values)
+print(missing_seat)
+
