c699214310863c00b2702857b79bab39a575b54e
David Graça <davidgraca@gmail.com>
2020-12-09 18:53:40 +0000

    solved regex

--- day4 second.py
+++ day4 second.py
@@ -21,8 +21,28 @@ for passport in passports:
     if len(firstchars)>= len(expected) and all(item in firstchars for item in expected):
         dict = {}
         list(map(lambda y : dict.update({y[0:3]:y[4:]}),passport))
-        tt =re.search("[1920-2002]",dict['byr'])
-        if re.search("[1920-2002]",dict['byr'])!= None and re.search("[2010-2020]",dict['iyr'])!= None and re.search("[2020-2030]",dict['eyr'])!= None and re.search("^([150-193]cm|[59-76]in)$",dict['hgt'])!= None and re.search("^#[0-9a-f]{6}", dict["hcl"])!= None and re.search("^(amb|blu|brn|gry|grn|hzl|oth)$", dict["hcl"])!= None and re.search("^#[0-9]{9}", dict["pid"])!= None:
+    
+        heigth = False
+        if dict['hgt'][len(dict['hgt'])-2:] =='in':
+            try:
+                value = int(dict['hgt'][:len(dict['hgt'])-2])
+                if 59 <= value<=76:heigth=True
+            except:
+                heigth=False
+        if dict['hgt'][len(dict['hgt'])-2:] =='cm':
+            try:
+                value = int(dict['hgt'][:len(dict['hgt'])-2])
+                if 150 <= value<=193:heigth=True
+            except:
+                heigth=False
+
+        byr=1920<= int(dict['byr'])<=2002
+        iyr= 2010 <= int(dict['iyr'])<=2020
+        eyr = 2020 <=int(dict['eyr'])<=2030
+        hcl = re.search("^\#[0-9a-f]{6}$", dict["hcl"])!= None
+        pid=re.search("^[0-9]{9}$", dict["pid"])!= None
+        ecl = re.search("^(amb|blu|brn|gry|grn|hzl|oth)$", dict["ecl"])!= None
+        if heigth and byr and iyr and eyr and hcl and ecl and pid:
             valid+=1
-re.search("([150-193]cm)|([59-75])",dict['hgt'])
+
 print(valid)                           # Print the list.
